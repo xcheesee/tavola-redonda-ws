5ef05b405d3658f13455916ce63131b13825a145
xcheesee <lucas121520@gmail.com>
2023-05-17 23:33:44 -0300

    implementado method de teste para implementacao de envio de pedido

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var logger = require('morgan');
 const http = require('http')
 var cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 const server = http.createServer(app)
 const { Server } = require("socket.io")
@@ -18,25 +15,18 @@ const io = new Server(server, {
   }
 })
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
 })
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -55,15 +45,11 @@ app.use(function(err, req, res, next) {
 
 io.on('connection', (socket) => {
   console.log("connected")
-  socket.on("clientHello", (message) => {
-    if( message === undefined) return
+  socket.on("pedidoRealizado", (message) => {
+    //if( message === undefined) return
     console.log(message)
   });
 });
-
-setInterval(() => {
-  io.emit('hello')
-}, 10000)
 
 server.listen(3000)
 
